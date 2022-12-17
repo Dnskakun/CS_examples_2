df98866f5b9cc67194ba7edccbd4df30a594b23b
dnskakun <dimon_sdn@list.ru>
2022-12-17 19:27:51 +0300

    Решение задачи 4

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Программа, показывающая все четные числа, находящиеся до введенного числа");
+Console.Write("Введите целое положительное число больше или равное 2: ");
+int userInput = int.Parse(Console.ReadLine() ?? "");
+
+int count = 1;
+
+Console.Write($"Все четные числа до числа {userInput}: ");
+
+while (count < userInput - 1)
+{
+    if (count%2 == 0)
+    {
+        Console.Write($"{count}, ");
+    }
+    count++;
+}
+while (count <= userInput)
+    {
+      if (count%2 == 0)
+      {
+        Console.WriteLine($"{count}.");
+      }
+      count++;
+    }
+    
+if (userInput < 2)
+{
+    Console.WriteLine("вы ввели число меньше 2, попробуйте заново.");
+}
