2861666cb2d0c9dc160327e4fe7180144c551026
dnskakun <dimon_sdn@list.ru>
2022-12-17 17:47:53 +0300

    Решение задачи 1

---
+++ Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое целое число: ");
+string a = Console.ReadLine() ?? "";
+Console.Write("Введите второе целое число: ");
+string b = Console.ReadLine() ?? "";
+
+if (int.Parse(a)>int.Parse(b)) //как лучше сделать: добавить две переменные (например, int number1 = int.Parse(a)) или можно сделать сразу в условии цикла метод Parse?
+{
+    Console.Write($"max={a}");
+}
+else
+{
+    Console.Write($"max={b}");
+}
