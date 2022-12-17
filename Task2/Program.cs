fdebe299995299f558a6fd0b650d0a0f0c0b2714
dnskakun <dimon_sdn@list.ru>
2022-12-17 18:05:29 +0300

    Решение задачи 2

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите первое целое число: ");
+int userInput1 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе целое число: ");
+int userInput2 = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите третье целое число: ");
+int userInput3 = int.Parse(Console.ReadLine() ?? "");
+
+int max = userInput1;
+
+if(userInput2 > max)
+{
+    max = userInput2;
+}
+else
+{
+    if(userInput3 > max)
+    {
+        max = userInput3;
+    }
+}
+
+Console.WriteLine($"Максимальным из введенных чисел является число {max}");
