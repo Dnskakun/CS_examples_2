c104b3ef9e8075d26bc857ef2e56925af1883e59
dnskakun <dimon_sdn@list.ru>
2022-12-17 17:50:52 +0300

    Решение задачи 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,9 +5,9 @@ string b = Console.ReadLine() ?? "";
 
 if (int.Parse(a)>int.Parse(b)) //как лучше сделать: добавить две переменные (например, int number1 = int.Parse(a)) или можно сделать сразу в условии цикла метод Parse?
 {
-    Console.Write($"max={a}");
+    Console.Write($"max={a}, min={b}");
 }
 else
 {
-    Console.Write($"max={b}");
+    Console.Write($"max={b}, min={a}");
 }
