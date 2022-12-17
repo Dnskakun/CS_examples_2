4cacb1f3a7232d7a79c45cb5760d5c3efcda8e7c
dnskakun <dimon_sdn@list.ru>
2022-12-17 18:40:50 +0300

    Решение задачи 3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Проверка числа на четность");
+Console.Write("Введите целое число: ");
+int userInput = int.Parse(Console.ReadLine() ?? "");
+
+if (userInput%2 == 0)
+{
+    Console.WriteLine($"Введенное число {userInput} является четным числом.");
+}
+else
+{
+    Console.WriteLine($"Введенное число {userInput} является нечетным числом");
+}
